155dc1040e40a3474998fcc2e80f5713c137ddbd
Rotarenco Leonid <hardcorequal@gmail.com>
2023-07-25 17:10:28 +0300

    fix position of action in FileMenu and also rename it to GitQuickView

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 <!-- Plugin description -->
 
-# IntelliJ IDEA Github Preview Plugin
+# IntelliJ IDEA Git QuickView Plugin
 
-This plugin adds an 'Open from GitHub' option in IntelliJ IDEA that lets you quickly clone a GitHub repository, open it in a new window, and automatically remove the repository when the project window is closed.
+This plugin adds an 'Open Git QuickView' option in IntelliJ IDEA that lets you quickly clone a Git repository, open it in a new window, and automatically remove the repository when the project window is closed.
 
 ## Features
 
-- **Clone and Open GitHub Repositories:** Paste a GitHub repository URL, and the plugin will clone the repository with a depth of 1 and open it in a new IntelliJ IDEA window.
+- **Clone and Open Git Repositories:** Paste a Git repository URL, and the plugin will clone the repository with a depth of 1 and open it in a new IntelliJ IDEA window.
 - **Automatic Cleanup:** When you're done with a repository and close the window, the plugin will automatically remove the repository from your computer. This keeps your workspace clean and prevents clutter from building up over time.
 
 ## Usage
 
-1. Go to `File > Open from GitHub...`.
-2. Paste the URL of a GitHub repository in the input field.
+1. Go to `File > Open for QuickView...`.
+2. Paste the URL of a Git repository in the input field.
 3. Click `OK`.
 
 <!-- Plugin description end -->
--- src/main/java/GitQuickViewAction.java
+++ src/main/java/GitQuickViewAction.java
@@ -1,102 +1,104 @@
 import com.esotericsoftware.minlog.Log;
 import com.intellij.ide.RecentProjectsManager;
-import com.intellij.ide.RecentProjectsManagerBase;
 import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.messages.MessageBusConnection;
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
-import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
 
-
-public class CloneForPreviewAction extends DumbAwareAction {
-    private static final Logger LOG = Logger.getInstance(CloneForPreviewAction.class);
+public class GitQuickViewAction extends DumbAwareAction {
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project currentProject = e.getProject();
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project currentProject = event.getProject();
 
-        String url = Messages.showInputDialog(
+        String gitUrl = Messages.showInputDialog(
                 currentProject,
-                "Enter the GitHub repository URL",
-                "Open from GitHub",
+                "Enter the Git repository URL",
+                "Open Git QuickView",
                 null
         );
 
-        File tempDir;
-        try {
-            tempDir = Files.createTempDirectory("MyPlugin").toFile();
-        } catch (IOException ex) {
-            // TODO: handle the exception
-            throw new RuntimeException(ex);
-        }
-
+        File tempDir = createTempDirectory();
         Log.info("Successfully created temporary directory");
 
-        // Clone the repository with depth 1
+        cloneGitRepo(gitUrl, tempDir);
+        Log.info("Successfully cloned the repository in the temporary directory: " + tempDir.getAbsolutePath());
+
+        File repoDir = getRepoDirectory(tempDir);
+        String repoPath = repoDir.getAbsolutePath();
+
+        Project newProject = ProjectUtil.openOrImport(repoPath, currentProject, true);
+        Log.info("Successfully opened the project in directory: " + repoPath);
+
+        cleanUpOnProjectClose(newProject, tempDir);
+
+        removeProjectFromRecentList(repoPath);
+        Log.info("Successfully removed the project from the recent projects list");
+    }
+
+    private File createTempDirectory() {
         try {
-            ProcessBuilder pb = new ProcessBuilder("git", "clone", "--depth", "1", url);
+            return Files.createTempDirectory("GitQuickView").toFile();
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to create temporary directory", ex);
+        }
+    }
+
+    private void cloneGitRepo(String gitUrl, File tempDir) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("git", "clone", "--depth", "1", gitUrl);
             pb.directory(tempDir);
             Process p = pb.start();
             p.waitFor();
         } catch (IOException | InterruptedException ex) {
-            // TODO: handle the exception
+            throw new RuntimeException("Failed to clone git repository", ex);
         }
+    }
 
-        Log.info("Successfully cloned the repository in the temporary directory: " + tempDir.getAbsolutePath());
-
+    private File getRepoDirectory(File tempDir) {
         File[] files = tempDir.listFiles();
 
-        // Get the first directory in the temp directory. This assumes that the cloned repo is the only thing in the temp directory.
-        File repoDir = Arrays.stream(files)
+        return Arrays.stream(files)
                 .filter(File::isDirectory)
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("No directory found in tempDir"));
+    }
 
-        String repoPath = repoDir.getAbsolutePath();
-
-        Project newProject = ProjectUtil.openOrImport(repoPath, currentProject, true);
-
-        Log.info("Successfully opened the project in directory: " + repoPath);
-
+    private void cleanUpOnProjectClose(Project newProject, File tempDir) {
         MessageBusConnection connection = ApplicationManager.getApplication().getMessageBus().connect();
 
         connection.subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
             @Override
-            public void projectClosing(Project project) {
+            public void projectClosing(@NotNull Project project) {
                 if (project.equals(newProject)) {
                     new Thread(() -> {
                         try {
-                            // Sleep for a bit to give IntelliJ time to finish its operations
-                            Thread.sleep(2000); // 2 seconds
-                            FileUtils.deleteDirectory(tempDir);
-
+                            Thread.sleep(2000); // Sleep for a bit to give IntelliJ time to finish its operations
+                            Files.delete(tempDir.toPath());
                             Log.info("Successfully deleted the temporary directory: " + tempDir.getAbsolutePath());
                         } catch (InterruptedException | IOException e) {
                             e.printStackTrace();
                         }
                     }).start();
-                    // Disconnect the message bus connection
                     connection.disconnect();
                 }
             }
         });
+    }
 
+    private void removeProjectFromRecentList(String repoPath) {
         RecentProjectsManager manager = RecentProjectsManager.getInstance();
         manager.removePath(repoPath);
-
-        Log.info("Successfully removed the project from the recent projects list");
-
     }
 }
